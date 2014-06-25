93f629554bad45df070487d7daf082431d366b5f
yeonoo <ywoo818@gmail.com>
2014-06-25 16:55:53 +0900

    7

--- src/app.js
+++ src/app.js
@@ -14,10 +14,20 @@ function autoLoad(event){
 	setHtmlCode( window.localStorage.getItem(selectedKey) );
 }
 
-
 function loadLocalStorage(event){
 	var value = $(event.currentTarget).val();
 	setHtmlCode( window.localStorage.getItem(value) );
+}
+
+function createNew(event){	
+	window.localStorage.setItem('note_contents', " ");
+	setHtmlCode( window.localStorage.getItem('note-contents'));
+}
+
+function saveLocalStorage(contents) {
+	var content = contents.code();
+	var oMyBlob = new Blob([content], { type : "text/plain", endings: "transparent"});
+	window.saveAs(oMyBlob, "note-test.html");
 }
 
 //-------------------------------------------------
--- src/button.js
+++ src/button.js
@@ -3,17 +3,6 @@ var bb = new BlobBuilder();
 bb.append(content);
 var fileSaver = window.saveAs(bb.getBlob("text/plain;charset=UTF-8"), "filename.txt");
 */
-function createNew(event){	
-	window.localStorage.setItem('note_contents', " ");
-	setHtmlCode( window.localStorage.getItem('note-contents'));
-}
-
-function saveLocalStorage(contents) {
-	var content = contents.code();
-	var oMyBlob = new Blob([content], { type : "text/plain", endings: "transparent"});
-	window.saveAs(oMyBlob, "note-test.html");
-}
-
 $('#fullscreen').click(function () {
 	screenfull.toggle($('#container')[0]);
 });
