e7b09479b47c75a5e828c5da6e50cd17062e7abc
yeonoo <ywoo818@gmail.com>
2014-06-25 16:01:33 +0900

    first

--- src/button.js
+++ src/button.js
@@ -1,1 +1,23 @@
-// 이곳에 코드를 작성하세요 
+/*
+var bb = new BlobBuilder();
+bb.append(content);
+var fileSaver = window.saveAs(bb.getBlob("text/plain;charset=UTF-8"), "filename.txt");
+*/
+var $btnCreateNew = $('#createNew');
+var $btnFullScreen = $('#fullScreen');
+var $btnSaveFile = $('#saveFile');
+var $btnRef = $('#reference');
+
+function createNew(event){	
+	window.localStorage.setItem('note_contents', " ");
+}
+
+function saveLocalStorage(contents) {
+	var content = contents.code();
+	var oMyBlob = new Blob([content], { type : "text/plain", endings: "transparent"});
+	window.saveAs(oMyBlob, "filename.txt");
+}
+
+$('#createNew').on('click', createNew);
+$('#fullScreen').on('click', screenfull);
+$('#saveFile').on('click', saveLocalStorage);
