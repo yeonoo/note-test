d3b7e8e088c601d82c152cadba8ec8ac11bcd785
yeonoo <ywoo818@gmail.com>
2014-06-25 18:42:32 +0900

    24

--- src/app.js
+++ src/app.js
@@ -17,6 +17,22 @@ function loadLocalStorage(event){
 	setHtmlCode( window.localStorage.getItem(value) );
 }
 
+function createNew(event){	
+	window.localStorage.setItem('note_contents', " ");
+	setHtmlCode( window.localStorage.getItem('note-contents'));
+}
+
+function fullscreen() {
+	if (screenfull.enabled) {
+		screenfull.request();
+	}	
+}
+
+function saveLocalStorage(contents) {
+	var content = contents.code();
+	var oMyBlob = new Blob([content], { type : "text/plain", endings: "transparent"});
+	window.saveAs(oMyBlob, "note-test.html");
+}
 //-------------------------------------------------
 
 
--- src/button.js
+++ src/button.js
@@ -1,24 +1,3 @@
-
-
-	var $contents = $('#contents');
-
-	function createNew(event){	
-		window.localStorage.setItem('note_contents', " ");
-		setHtmlCode( window.localStorage.getItem('note-contents'));
-	}
-
-	function fullscreen() {
-		if (screenfull.enabled) {
-			screenfull.request();
-		}	
-	}
-
-	function saveLocalStorage(contents) {
-		var content = contents.code();
-		var oMyBlob = new Blob([content], { type : "text/plain", endings: "transparent"});
-		window.saveAs(oMyBlob, "note-test.html");
-	}
-
-	$('#createNew').on('click', createNew);
-	$('#fullScreen').on('click', fullScreen);
-	$('#saveFile').on('click', saveLocalStorage);
+$('#createNew').on('click', createNew);
+$('#fullScreen').on('click', fullScreen);
+$('#saveFile').on('click', saveLocalStorage);
