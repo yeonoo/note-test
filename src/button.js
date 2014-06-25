99d6fcb07d743b5845ce1b29cc2d56b9652f8df7
yeonoo <ywoo818@gmail.com>
2014-06-25 17:15:39 +0900

    rewq

--- src/button.js
+++ src/button.js
@@ -3,15 +3,35 @@ var bb = new BlobBuilder();
 bb.append(content);
 var fileSaver = window.saveAs(bb.getBlob("text/plain;charset=UTF-8"), "filename.txt");
 */
+
+function fullScreen() {
+	var el = document.documentElement
+   		, rfs = // for newer Webkit and Firefox
+           el.requestFullScreen
+        || el.webkitRequestFullScreen
+        || el.mozRequestFullScreen
+        || el.msRequestFullScreen
+	;
+	if(typeof rfs!="undefined" && rfs){
+  	rfs.call(el);
+	} else if(typeof window.ActiveXObject!="undefined"){
+  	// for Internet Explorer
+  		var wscript = new ActiveXObject("WScript.Shell");
+  		if (wscript!=null) {
+   			wscript.SendKeys("{F11}");
+  		}
+	}
+}
+
 $('#fullscreen').click(function () {
 	if (document.fullscreenEnabled) {
-        requestFullscreen(document.documentElement);
+        fullScreen();
     };
 });
 
 
 $('#createNew').click(function() {
-	createNew()
+	createNew();
 });
 
 $('#saveFile').click(function() {
